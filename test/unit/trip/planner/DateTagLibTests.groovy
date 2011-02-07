944618833c712e59a1eda91ad49e1868771a9253
Aaron Robinson <aaronlrobinson@gmail.com>
2011-02-07 16:13:36 -0700

    Initial add.

---
+++ test/unit/trip/planner/DateTagLibTests.groovy
@@ -0,0 +1,23 @@
+package trip.planner
+
+import grails.test.*
+
+class DateTagLibTests extends TagLibUnitTestCase {
+
+    def dateTagLib;
+
+    protected void setUp() {
+        super.setUp()
+
+        dateTagLib = new DateTagLib();
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testThisYear() {
+            String expected = Calendar.getInstance().get(Calendar.YEAR)
+            assertEquals("the years don't match", expected, (String)dateTagLib.thisYear())
+    }
+}
